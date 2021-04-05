8fecd873d558d9e0911e4ab8b55b9516ce47f429
ryotak1998 <74517286+ryotak1998@users.noreply.github.com>
2021-04-05 22:48:30 +0900

    トップページに戻るボタンの作成

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
 "use strict";
+
+{
+    
+    window.addEventListener('DOMContentLoaded', function () {
+
+        //ページトップに戻る
+        //ボタンの表示
+        const to_top = document.querySelector('.send_to_top');
+
+        // to_top.addEventListener('transitionrun', function () {
+        //     if (to_top.className !== 'view') {
+        //         to_top.style.visibility = 'hidden';
+        //     }
+        // });
+        
+        window.addEventListener('scroll', function () {
+            if (200 < window.scrollY) {
+                to_top.style.visibility = 'visible';
+                to_top.style.cursor = 'pointer';
+                to_top.classList.add('view');
+                // console.log(window.scrollY);
+            } else {
+                to_top.classList.remove('view');
+                to_top.style.cursor = 'default';
+            }
+        });
+
+        //スクロールでトップへ移動
+        to_top.addEventListener('click', function () {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
+}
