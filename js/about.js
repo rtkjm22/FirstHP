12c852325b475f5899d6b68bca0138bd3fba4764
ryotak1998 <74517286+ryotak1998@users.noreply.github.com>
2021-04-12 17:55:23 +0900

    aboutの追加

--- js/about.js
+++ js/about.js
@@ -1,5 +1,27 @@
 "use strict";
 
 {
+    const item = document.querySelectorAll('.about_item');
+
+
+    function scroll_display() {
+        for (let i=0;i<item.length;i++) {
+            const rect = item[i].getBoundingClientRect().top;
+            const gap = item[i].clientHeight * .2;
+            if (window.innerHeight > rect + gap) {
+                item[i].classList.add('scroll_display');
+            } 
+        }
+    }
+
+
+    window.addEventListener('load', () => {
+        scroll_display();
+    });
+
+    window.addEventListener('scroll', () => {
+        scroll_display();
+    });
+    
     
 }
